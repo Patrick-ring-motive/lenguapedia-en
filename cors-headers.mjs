52073992ad9309073ca5ab2097a73d904d780cfb
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:26:37 -0500

    style: js-beautify formatting (cors-headers.mjs)

--- cors-headers.mjs
+++ cors-headers.mjs
@@ -1,4 +1,3 @@
-
 export default function addCorsHeaders(res) {
   res.removeHeader('Access-Control-Allow-Origin');
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -11,16 +10,16 @@ export default function addCorsHeaders(res) {
   res.removeHeader('Access-Control-Max-Age');
   res.setHeader('Access-Control-Max-Age', '86400');
   res.removeHeader('Referrer-Policy');
-//  res.setHeader('Referrer-Policy', 'unsafe-url');
+  //  res.setHeader('Referrer-Policy', 'unsafe-url');
   res.removeHeader('Content-Security-Policy');
-//  res.setHeader('Content-Security-Policy', "upgrade-insecure-requests;");
+  //  res.setHeader('Content-Security-Policy', "upgrade-insecure-requests;");
   res.removeHeader('X-Frame-Options');
   res.removeHeader('Strict-Transport-Security');
   res.removeHeader('X-Content-Type-Options');
   res.removeHeader('Cross-Origin-Embedder-Policy');
   res.removeHeader('Cross-Origin-Resource-Policy');
-//  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+  //  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   res.removeHeader('Cross-Origin-Opener-Policy');
-// res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none');
+  // res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none');
   return res;
 }
